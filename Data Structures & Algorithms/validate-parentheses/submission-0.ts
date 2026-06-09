4a8d5c2a42761b9f7971049bcaa923eed9d97c3e
Mendo <nicolasmendonca@gmail.com>
2026-06-09 16:06:42 -0300

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s: string): boolean {
+        let bracketStack: string[] = [];
+        let bracketPairs = {
+            '[': ']',
+            '(': ')',
+            '{': '}'
+        }
+
+        for (const char of s) {
+            
+            // bracket is opening
+            if (char in bracketPairs) {
+                bracketStack.push(char);
+                continue;
+            }
+
+            // bracket is closing
+            const lastOpenedBracket = bracketStack.at(-1);
+            const previousBracketExists = !!lastOpenedBracket;
+            const currentCharClosesLastOpenedBracket = bracketPairs[lastOpenedBracket] === char
+            if (previousBracketExists && currentCharClosesLastOpenedBracket) {
+                bracketStack.pop();
+                continue;
+            }
+
+            // valid conditions were not met
+            return false;
+        }
+
+        return true;
+    }
+}
